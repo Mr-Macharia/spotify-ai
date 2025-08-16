2f86cc6dd4856742c8c8571d151f9817f259a1ae
Mr-Macharia <gichogumacharia001@gmail.com>
2025-08-17 02:59:27 +0300

    pages

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import { GoSearch } from "react-icons/go";
+import { MdHomeFilled } from "react-icons/md";
+
+export default function Navbar(){
+    return (
+        <nav className="h-15 flex justify-between items-center px-6 fixed top-0 left-0 w-full bg-black z-100">
+            <div className="flex gap-6 items-center">
+                <Image src="/images/logo.png" alt="logo-image" width={500} height={500} className="w-9 h-9" />
+                <Link href="/" className="bg-background w-11 h-11 grid place-items-center text-white text-3x1 rounded-full">
+                    <MdHomeFilled className="w-9 h-9" />
+                </Link>
+                <div className="bg-background hidden lg:flex flex items-center h-11 w-90 px-3 gap-3 text-primary-text rounded-full">
+                    <GoSearch className="text-primary text shrink-0"/>
+                    <input className="h-full w-full outline-none place-holder:text-primary-text" type='text' placeholder="What do you want to play?" />
+                </div>
+            </div>
+
+            <div className="flex items-center gap-8">
+                <div className="lg:flex hidden gap-2 text-secondary-text font-bold border-r-2 border-primary-text
+                pr-6">
+                    <a href="#" className="hover:text-primary-text">
+                        Premium
+                    </a>
+                    <a href="#" className="hover:text-primary-text">
+                        Support
+                    </a>
+                    <a href="#" className="hover:text-primary-text">
+                        Download
+                    </a>
+
+                </div>
+                <div>
+                    <Link href="/login" className="h-11 bg-white text-gray-950 rounded-full font-bold hover:bg-secondary-text grid px-8 place-items-center">
+
+                        Login
+                    </Link>
+                </div>
+            </div>
+
+        </nav>
+    )
+}
+
+
+
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,136 @@
+"use client";
+import React, { useState, useEffect } from "react"
+import Link from "next/link"
+import Image from "next/image"
+import { LiaPlusCircleSolid } from "react-icons/lia"
+import { LuPlus } from "react-icons/lu"
+import { MdOutlineLibraryMusic } from "react-icons/md";
+
+
+export default function Sidebar() {
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 500) {
+        setSidebarOpen(true);
+      } else {
+        setSidebarOpen(false);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
+    <div>
+        <aside className={`fixed left-2 top-15 bg-background w-75 rounded-lg h-[90vh]
+        p-2 over-y-auto transition-transform duration-600 ease-in-out ${sidebarOpen ? "translate-x-0" : "-translate-x-full"}`}>
+            <div className="flex justify-between text-primary-text items-center p-2 mb-4">
+                <h2 className="font-bold">Your Library</h2>
+                <Link href="/upload-song">
+                <LuPlus size="20"/>
+                </Link>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="flex gap-2 items-center cursor-pointer mb-4 p-2 rounded-lg hover:bg-hover">
+                <Image
+                  src="/images/cover-1.jpeg"
+                  alt='cover-image'
+                  width={300}
+                  height={300}
+                  className='w-10 h-10 object-cover rounded-lg'/>
+                
+                <div>
+                  <p className="text-primary-text font-semibold">Midnight Echoes</p>
+                  <p className="text-secondary-text text-sm">Neon Skyline</p>
+                </div>
+                
+
+              </div>
+
+               <div className="flex gap-2 items-center cursor-pointer mb-4 p-2 rounded-lg hover:bg-hover">
+                <Image
+                  src="/images/cover-2.jpeg"
+                  alt='cover-image'
+                  width={300}
+                  height={300}
+                  className='w-10 h-10 object-cover rounded-lg'/>
+                
+                <div>
+                  <p className="text-primary-text font-semibold">Jamaican Jersey</p>
+                  <p className="text-secondary-text text-sm"> By The band</p>
+                </div>
+                
+
+              </div>
+
+               <div className="flex gap-2 items-center cursor-pointer mb-4 p-2 rounded-lg hover:bg-hover">
+                <Image
+                  src="/images/cover-3.jpeg"
+                  alt='cover-image'
+                  width={300}
+                  height={300}
+                  className='w-10 h-10 object-cover rounded-lg'/>
+                
+                <div>
+                  <p className="text-primary-text font-semibold">Lost Feelings</p>
+                  <p className="text-secondary-text text-sm">By Spooky System</p>
+                </div>
+                
+
+              </div>
+
+               <div className="flex gap-2 items-center cursor-pointer mb-4 p-2 rounded-lg hover:bg-hover">
+                <Image
+                  src="/images/cover-4.jpeg"
+                  alt='cover-image'
+                  width={300}
+                  height={300}
+                  className='w-10 h-10 object-cover rounded-lg'/>
+                
+                <div>
+                  <p className="text-primary-text font-semibold">Little Castle</p>
+                  <p className="text-secondary-text text-sm">By Chess Endevour</p>
+                </div>
+                
+
+              </div>
+
+               <div className="flex gap-2 items-center cursor-pointer mb-4 p-2 rounded-lg hover:bg-hover">
+                <Image
+                  src="/images/cover-5.jpeg"
+                  alt='cover-image'
+                  width={300}
+                  height={300}
+                  className='w-10 h-10 object-cover rounded-lg'/>
+                
+                <div>
+                  <p className="text-primary-text font-semibold">Midnight Echoes</p>
+                  <p className="text-secondary-text text-sm">By Jump Street</p>
+                </div>
+                
+
+              </div>
+
+            </div>
+
+        </aside>
+
+        <button onClick={() => setSidebarOpen(!sidebarOpen)} className="fixed md:hidden bottom-5 left-5 bg-background h-12 w-12 grid place-items-center
+        text-white rounded-full z-50 cursor-pointer">
+
+          <MdOutlineLibraryMusic/>
+
+        </button>
+    </div>
+  )
+}
